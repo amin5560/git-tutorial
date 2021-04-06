1b922eef39e8ab8659d90cf85a74ad2068433278
Amin <aminaminurrahman@gmail.com>
2021-04-06 18:27:56 +0600

    Added a new method called disply to hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,10 @@
 public class Hello {
     public static void main (string [] args){
-        system.out.printIn("Vs code is Awesome...")
+        system.out.printIn("Vs code is Awesome...");
+        greetings("Aminur Rahman")
+    }
+
+    public static void greetings(string name){
+        system.out.printIn("Welcome" + name);
     }
 }
